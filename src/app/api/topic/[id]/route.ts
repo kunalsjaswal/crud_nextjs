1fa00160f58d930d224db167109b16e011b654bd
kunalsjaswal <kunal23singh0@gmail.com>
2025-04-13 10:55:04 +0530

    backend api change

---
+++ src/app/api/topic/[id]/route.ts
@@ -0,0 +1,150 @@
+import { NextResponse } from "next/server";
+import { Topic } from "../../../../../models/topic";
+import connectMongooDB from "../../../../../libs/mongodb";
+
+export const DELETE = async (request: Request, { params }: { params: { id: string } }) => {
+  try {
+
+    const { id } = params;
+
+    if (!id) {
+      return NextResponse.json(
+        {
+          status: 400,
+          data: null,
+          message: "Topic ID is required",
+        },
+        { status: 400 }
+      );
+    }
+
+    
+    await connectMongooDB();
+
+    const topic = await Topic.findByIdAndDelete(id);
+    if (!topic) {
+      return NextResponse.json(
+        {
+          status: 404,
+          data: null,
+          message: "Topic not found",
+        },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      {
+        status: 200,
+        data: null,
+        message: "Topic deleted successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        status: 500,
+        data: null,
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+};
+
+
+export const PUT = async (request: Request, { params }: { params: { id: string } }) => {
+  try {
+    const { id } = params;
+    const { title, description } = await request.json();
+    await connectMongooDB();
+
+    const topic = await Topic.findByIdAndUpdate(
+      id,
+      { title, description },
+      { new: true }
+    );
+    if (!topic) {
+      return NextResponse.json(
+        {
+          status: 404,
+          data: null,
+          message: "Topic not found",
+        },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      {
+        status: 200,
+        data: topic,
+        message: "Topic updated successfully",
+      },
+      { status: 200 }
+    );
+    
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        status: 500,
+        data: null,
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+
+export const GET = async (request: Request, { params }: { params: { id: string } }) => {
+  try {
+
+    const { id } =  params;
+    if (!id) {
+      return NextResponse.json(
+        {
+          status: 400,
+          data: null,
+          message: "Topic ID is required",
+        },
+        { status: 400 }
+      );
+    }
+
+    await connectMongooDB();
+
+    const topic = await Topic.findById(id);
+    if (!topic) {
+      return NextResponse.json(
+        {
+          status: 404,
+          data: null,
+          message: "Topic not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        status: 200,
+        data: topic,
+        message: "Topic fetched successfully",
+      },
+      { status: 200 }
+    );
+    
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        status: 500,
+        data: null,
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/topic/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+import connectMongooDB from "../../../../libs/mongodb";
+import { Topic } from "../../../../models/topic";
+import { request } from "http";
+
+export const POST = async (request: Request) => {
+  try {
+    const { title, description } = await request.json();
+
+    await connectMongooDB();
+
+    await Topic.create({ title, description });
+
+    return NextResponse.json(
+      {
+        message: "Topic created successfully",
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+};
+
+export const GET = async (request: Request) => {
+  try {
+    await connectMongooDB();
+    const topics = await Topic.find();
+    return NextResponse.json(
+      {
+        status: 200,
+        data: {
+          count: topics.length,
+          topics,
+        },
+        timestamp: new Date().toISOString(),
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        status: 500,
+        data: null,
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
+};
